00c7aa1370190eac9d9d16fe8cb756779ecc7695
RainbowBird <rbxin2003@outlook.com>
2020-09-01 21:44:28 +0800

    Site updated: 2020-09-01 21:44:27

--- js/funnytitle.js
+++ js/funnytitle.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/assets/js/Meting.min.js"></script>// 浏览器搞笑标题
+// 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
